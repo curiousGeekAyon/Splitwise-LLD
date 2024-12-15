5159c083a42a29ee85206fd4919048c875f6af5e
curiousGeekAyon~ <ayon1234kgp@gmail.com>
2024-12-15 19:11:29 +0530

    minor mistake

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import static jdk.internal.org.jline.terminal.spi.TerminalProvider.Stream.Input;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
